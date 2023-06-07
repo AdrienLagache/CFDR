18ef06bf80bad7dc003a78816db8c1bbf3d00b59
AdrienLagache <adrien.lagache@oclock.school>
2023-06-07 17:06:02 +0200

    ajout link fontAwesome dans header

--- app/views/header.tpl.php
+++ app/views/header.tpl.php
@@ -6,6 +6,7 @@
 
     <link rel="stylesheet" href="assets/css/reset.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
     <link rel="stylesheet" href="assets/css/style.css">
     <title>CFDR</title>
 </head>
